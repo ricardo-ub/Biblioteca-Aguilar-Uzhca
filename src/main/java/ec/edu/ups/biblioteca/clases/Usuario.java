dc99b6123300df3a595bbc25600932ea24ab8007
Ricardo Uzhca <ricardouzhcab27@gmail.com>
2026-05-10 06:36:58 -0500

    Primer commit - biblioteca

---
+++ src/main/java/ec/edu/ups/biblioteca/clases/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.biblioteca.clases;
+
+import java.util.List;
+
+/**
+ *
+ * @author DELL
+ */
+public class Usuario extends Persona{
+    private List<Prestamo> prestamos;
+    private int numeroCedula ;
+
+    public Usuario() {
+    }
+
+    public Usuario(List<Prestamo> prestamos, int numeroCedula) {
+        this.prestamos = prestamos;
+        this.numeroCedula = numeroCedula;
+    }
+
+    public Usuario(List<Prestamo> prestamos, int numeroCedula, String nombre, String correo) {
+        super(nombre, correo);
+        this.prestamos = prestamos;
+        this.numeroCedula = numeroCedula;
+    }
+
+    public List<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(List<Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+    public int getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(int numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "prestamos=" + prestamos + ", numeroCedula=" + numeroCedula + '}';
+    }
+    
+}
